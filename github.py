485cf3dff89f8cca0826953d29a7e3b74e321aef
karankartikeya <karankartikey72@gmail.com>
2022-07-29 19:05:27 +0530

    fix:function error

--- github.py
+++ github.py
@@ -50,6 +50,6 @@ class Github:
         """Returns the data of the repository"""
         data = {'created_since_months': self.created_since_months(), 'updated_since_months': self.updated_since_months(),
                 'forks': self.get_forks_count(),
-                'contributors': self.get_contributors_count(), 'commit_frequency': self.get_commits_count()/(self.get_created_since_months*4)
+                'contributors': self.get_contributors_count(), 'commit_frequency': self.get_commits_count()/(self.created_since_months*4),
                 }
         return data
